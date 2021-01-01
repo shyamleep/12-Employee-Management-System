822e531452fa81587b7f2358b579aafa9dd7e489
Venn Diagram <68201342+shyamleep@users.noreply.github.com>
2020-12-31 21:09:11 -0500

    schema done

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS mortar_db;
+CREATE DATABASE mortar_db;
+
+USE DATABASE mortar_db;
+
+CREATE TABLE  employees (
+    id INT(3) NOT NULL AUTO_INCREMENT ZEROFILL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT(2) NOT NULL AUTO_INCREMENT ZEROFILL,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL(10,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    dept VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+
---
+++ seed.sql
@@ -0,0 +1,6 @@
+USE mortar_db;
+
+INSERT INTO departments (id, dept) VALUES
+()
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id, department_id) VALUES
